83a6f3565183c0c149b4a2d90d4b911f552c7d28
Adejobi Adedayo Israel <128178213+dayreachboi@users.noreply.github.com>
2024-04-01 15:28:59 +0100

    Add files via upload

---
+++ News Letter/JS/main.js
@@ -0,0 +1,13 @@
+function validateEmail() {
+  var email = document.getElementById("emailInput").value;
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (regex.test(email)) {
+    window.location.href =
+      "success%20page/index.html";
+  } else {
+    document.getElementById("errorMsg").style.display = "block";
+    emailInput.style.borderColor = "red";
+    emailInputField.style.color = "red";
+  }
+}
